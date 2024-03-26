ade980a8d34100ed6ce0d4ddb20f16ab23256afd
Syahfrianss <syahfrianarrafi@gmail.com>
2024-03-26 15:03:47 +0700

    first commit

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 const makanan1 = 10000;
 const makanan2 = 25000;
 const makanan3 = 15000;
-let total = makanan1 + makanan2 + makanan3;
+const makanan4 = 12000;
+let total = makanan1 + makanan2 + makanan3 + makanan4;
 let diskon = 0.10 * total ;
 let totalpembayaran = total - diskon;
 document.getElementById('demo1').innerHTML = "Total Bayar = Rp " + total;
-document.getElementById('demo2').innerHTML = "Total Bayar Diskon 10% = Rp " + totalpembayaran;
+document.getElementById('demo2').innerHTML = "Total Diskon 10% = Rp " + diskon;
+document.getElementById('demo3').innerHTML = "Total Pembayaran = Rp " + totalpembayaran;
