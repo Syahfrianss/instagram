b951fdeed2752f7837845239fc65db5db27d313f
Syahfrianss <syahfrianarrafi@gmail.com>
2024-03-31 16:47:50 +0700

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,103 @@
-let menuharga = {
-    menu1: {menu: 'Es Kopi Susu', harga: 10000},
-    menu2: {menu: 'Roti Bakar', harga: 25000},
-    menu3: {menu: 'Tahu Telor', harga: 15000},
-    menu4: {menu: 'Nasi Goreng', harga: 12000},
+//Tugas 4
+let mnhrg = {
+  nasipecel: { 
+    nama1: "Nasi Pecel",
+    harga1: 10000, 
+    varian:["Pedas Banget", "Pedas", "Pedas Original"], 
+    level:["Lvl 10","Lvl 9","Lvl 8","Lvl 7","Lvl 6","Lvl 5","Lvl 4","Lvl 3","Lvl 2","Lvl 1"]
+  },
+  nasijagung: { 
+    nama1:"Nasi Jagung", 
+    harga1: 25000, 
+    varian:["Pedas Banget", "Pedas", "Pedas Original"],
+    level: ["Lvl 10","Lvl 9","Lvl 8","Lvl 7","Lvl 6","Lvl 5","Lvl 4","Lvl 3","Lvl 2","Lvl 1"]
+  },
+  nasiayam: { 
+    nama1:"Nasi Ayam", 
+    harga1: 15000, 
+    varian:["Pedas Banget", "Pedas", "Pedas Original"],
+    level:["Lvl 10","Lvl 9","Lvl 8","Lvl 7","Lvl 6","Lvl 5","Lvl 4","Lvl 3","Lvl 2","Lvl 1"]
+  },
+  nasikrawu: { 
+    nama1:"Nasi Krawu", 
+    harga1: 12000, 
+    varian:["Pedas Banget", "Pedas", "Pedas Original"], 
+    level:["Lvl 10","Lvl 9","Lvl 8","Lvl 7","Lvl 6","Lvl 5","Lvl 4","Lvl 3","Lvl 2","Lvl 1"]
+  },
+  nasirendang: { 
+    nama1:"Nasi Rendang", 
+    harga1: 15000, 
+    varian:["Pedas Banget", "Pedas", "Pedas Original"],  
+    level:["Lvl 10","Lvl 9","Lvl 8","Lvl 7","Lvl 6","Lvl 5","Lvl 4","Lvl 3","Lvl 2","Lvl 1"]
+  },
+  nasilele: { 
+    nama1:"Nasi Lele", 
+    harga1: 13000, 
+    varian:["Pedas Banget", "Pedas", "Pedas Original"],   
+    level:["Lvl 10","Lvl 9","Lvl 8","Lvl 7","Lvl 6","Lvl 5","Lvl 4","Lvl 3","Lvl 2","Lvl 1"]
+  },
 };
-document.getElementById('d9').innerHTML = menuharga.menu1.menu + " Rp " + menuharga.menu1.harga;
-document.getElementById('d10').innerHTML = menuharga.menu2.menu + " Rp " + menuharga.menu2.harga;
-document.getElementById('d11').innerHTML = menuharga.menu3.menu + " Rp " + menuharga.menu3.harga;
-document.getElementById('d12').innerHTML = menuharga.menu4.menu + " Rp " + menuharga.menu4.harga;
+mnhrg.nasipecel.promo = 5000;
+const pesanan = ["nasipecel","nasijagung","nasirendang"];
+let totalB = 0;
+for (const i of pesanan) {
+  totalB += mnhrg[i].harga1;
+  if (mnhrg[i].promo) {
+    totalB -= mnhrg[i].promo;
+  }
+}
+const member = true;
+if (member) {
+  let diskonB = 0.1 * totalB;
+  let pajak = 0.11 * totalB;
+  totalbayarB = totalB - diskonB + pajak;
+}
+document.getElementById("diskon").innerHTML = "Diskon: Rp " + diskonB;
+document.getElementById("pajak").innerHTML = "Pajak: Rp ";
+document.getElementById("bayar").innerHTML = "Total Bayar: Rp " + totalbayarB;
+/*
+let totalB = 0;
+let menuB = "";
+for (let i = 0; i < mnhrg.length; i++) {
+menuB += `<li>${mnhrg[i].nama1}: Rp ${mnhrg[i].harga1}</li>`;
+totalB += mnhrg[i].harga1;
+}
+let diskonB = 0.1 * totalB;
+let totalpembayaranB = totalB - diskonB;
+
+document.getElementById("menuB").innerHTML = menuB;
+document.getElementById("d16").innerHTML = "Total = Rp " + totalB;
+document.getElementById("d17").innerHTML = "Total Diskon = Rp " + diskonB;
+document.getElementById("d18").innerHTML = "Total Pembayaran = Rp " + totalpembayaranB;
+*/
+
+/* TUGAS 3 Object */
+let menuharga = [
+    { namamenu: "Es Kopi Susu", harga: 10000},
+    { namamenu:"Roti Bakar", harga: 25000},
+    { namamenu:"Tahu Telor", harga: 15000},
+    { namamenu:"Nasi Goreng", harga: 12000},
+];
+//document.getElementById('d9').innerHTML = menuharga.menu1.menu + " Rp " + menuharga.menu1.harga;
+//document.getElementById('d10').innerHTML = menuharga.menu2.menu + " Rp " + menuharga.menu2.harga;
+//document.getElementById('d11').innerHTML = menuharga.menu3.menu + " Rp " + menuharga.menu3.harga;
+//document.getElementById('d12').innerHTML = menuharga.menu4.menu + " Rp " + menuharga.menu4.harga;
 
 let totalA = 0;
-for (let menu in menuharga) {
-  totalA += menuharga[menu].harga;
+let menuA = "";
+for (let i = 0; i < menuharga.length; i++) {
+  menuA += `<li>${menuharga[i].namamenu}: Rp ${menuharga[i].harga}</li>`;
+  totalA += menuharga[i].harga;
 }
 let diskonA = 0.1 * totalA;
 let totalpembayaranA = totalA - diskonA;
 
+document.getElementById("menuA").innerHTML = menuA;
 document.getElementById("d13").innerHTML = "Total = Rp " + totalA;
 document.getElementById("d14").innerHTML = "Total Diskon = Rp " + diskonA;
 document.getElementById("d15").innerHTML = "Total Pembayaran = Rp " + totalpembayaranA;
 
-
+/*Tugas 2 */
 const menutakjil = ['Es Kopi Susu', 'Roti Bakar', 'Tahur Telor', 'Nasi Goreng'];
 menutakjil[3] = 'Nasi Pecel';
 menutakjil[4] = 'Ayam Goreng';
@@ -42,6 +119,7 @@ document.getElementById('d6').innerHTML = "Total = Rp " + total;
 document.getElementById('d7').innerHTML = "Total Diskon = Rp " + diskon;
 document.getElementById('d8').innerHTML = "Total Bayar = Rp " + totalpembayaran;
 
+/*Tugas 1 */
 const menu = [10000, 25000, 15000];
 const makanan1 = 10000;
 const makanan2 = 25000;
